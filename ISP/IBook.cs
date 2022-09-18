38f5c7630e4de6f6c5ad21c66edb99046818037a
hamdinawfel <hamdii.nawfel@gmail.com>
2022-09-18 15:28:30 +0100

    refactor by implementing ISP using small interfaces insted of one fat interface

---
+++ ISP/IBook.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoLibrary;
+
+public interface IBook
+{
+    string LibraryId { get; set; }
+    string Title { get; set; }
+    string Author { get; set; }
+    int Pages { get; set; }
+    int CheckOutDurationInDays { get; set; }
+    string Borrower { get; set; }
+    DateTime BorrowDate { get; set; }
+    void CheckOut(string borrower);
+    void CheckIn();
+    DateTime GetDueDate();
+}
---
+++ ISP/IDvd.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoLibrary;
+
+public interface IDvd
+{
+    string LibraryId { get; set; }
+    string Title { get; set; }
+    int CheckOutDurationInDays { get; set; }
+    string Borrower { get; set; }
+    DateTime BorrowDate { get; set; }
+    int RuntimeInMinutes { get; set; }
+    void CheckOut(string borrower);
+    void CheckIn();
+    DateTime GetDueDate();
+}
